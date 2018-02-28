aa4b4c19f00f58e16b869ece20b554b84ac03af7
wmzy <1256573276@qq.com>
2018-02-28 21:53:35 +0800

    refactor file

--- file-cache-manager.js
+++ file-cache-manager.js
@@ -24,91 +24,20 @@ class FileCacheManager {
   }
 
   getWriteStream(key) {
-    if (this.fileCache.has(key)) throw new Error('file is writing');
+    if (this.fileCache.has(key)) throw new Error('file is exists');
     const file = new File(path.join(this.filePath, Buffer.from(key).toString('hex')));
     this.fileCache.set(key, file);
-    const writeStream = fs.createWriteStream(file.filename);
-    writeStream.on('error', e => {
+    const writeStream = file.createWriteStream();
+    writeStream.once('error', e => {
       this.fileCache.del(key);
-      file.error = e;
-      file.status = 'error';
-      file.emit('change');
     });
-
-    const self = this;
-    return miss.to({
-      destroy(err) {
-        self.fileCache.del(key);
-        writeStream.destroy(err);
-        file.status = 'error';
-        file.emit('change');
-      }
-    }, function (chunk, enc, callback) {
-      const ok = writeStream.write(chunk, enc, () => {
-        file.size += chunk.length;
-        file.emit('change');
-      });
-      if (ok) return callback();
-      writeStream.once('drain', callback);
-    }, function(callback) {
-      writeStream.end(() => {
-        debug('write done');
-        if (file.status === 'writing') file.status = 'done';
-        callback();
-        file.emit('change');
-      })
-    });
+    return writeStream;
   }
 
   createReadStream(key) {
     const file = this.fileCache.get(key);
     if (!file) throw new Error('file not found');
-    if (file.status === 'done') return fs.createReadStream(file.filename);
-    let fd;
-    let bytesRead = 0;
-    let closeFD = function (cb) {
-      if (fd !== undefined) {
-        fs.close(fd, cb)
-        closeFD = function noop() {};
-      }
-    }
-    return miss.from({
-      destroy(err, cb) {
-        closeFD(cb)
-      }
-    }, function readFromFile(size, next) {
-      if (file.status === 'error') {
-        return next(new Error('file error on writing:' + file.error));
-      }
-      if (bytesRead >= file.size && file.status === 'done') {
-        debug('write done');
-        return closeFD(e => next(e, null));
-      }
-      if (bytesRead + size >= file.size && file.status !== 'done') {
-        debug('file is writing, wait change');
-        return file.once('change', () => readFromFile(size, next))
-      }
-      if (fd !== undefined) return read(fd)
-      fs.open(file.filename, 'r', (err, _fd) => {
-        debug('open file to read');
-        if (err) return next(err);
-        fd = _fd;
-        debug('file is open');
-        return read(fd);
-      })
-
-      function read(fd) {
-        const length = Math.min(size, file.size - bytesRead);
-        fs.read(fd, Buffer.alloc(length), 0, length, null, (err, br, buffer) => {
-          if (err) {
-            return next(err)
-          }
-          bytesRead += br;
-          debug('read size:', br);
-          next(null, buffer)
-        })
-      }
-    })
+    return file.createReadStream();
   }
 }
 
--- file.js
+++ file.js
@@ -1,15 +1,97 @@
 const fs = require('fs');
 const util = require('util');
+const miss = require('mississippi');
 const EventEmitter = require('events');
 
 const openFile = util.promisify(fs.open);
+const debug = util.debuglog('file');
 
 class File extends EventEmitter {
   constructor(filename) {
     super();
-    this.status = 'writing';
+    this.status = 'init';
     this.size = 0;
     this.filename = filename;
+  }
+
+  createWriteStream() {
+    if (this.status !== 'init') throw new Error('file is exists');
+    this.status = 'writing';
+    const writeStream = fs.createWriteStream(this.filename);
+
+    const self = this;
+    return miss.to({
+      destroy(err) {
+        writeStream.destroy(err);
+        self.status = 'error';
+        self.error = err;
+        self.emit('change');
+      }
+    }, function (chunk, enc, callback) {
+      const ok = writeStream.write(chunk, enc, () => {
+        self.size += chunk.length;
+        self.emit('change');
+      });
+      if (ok) return callback();
+      writeStream.once('drain', callback);
+    }, function(callback) {
+      writeStream.end(() => {
+        debug('write done');
+        if (self.status === 'writing') self.status = 'done';
+        callback();
+        self.emit('change');
+      })
+    });
+  }
+
+  createReadStream() {
+    if (this.status === 'done') return fs.createReadStream(this.filename);
+    let fd;
+    let bytesRead = 0;
+    let closeFD = function (cb) {
+      if (fd !== undefined) {
+        fs.close(fd, cb)
+        closeFD = function noop() {};
+      }
+    }
+    const self = this;
+    return miss.from({
+      destroy(err, cb) {
+        closeFD(cb)
+      }
+    }, function readFromFile(size, next) {
+      if (self.status === 'error') {
+        return next(new Error('file error on writing:' + self.error));
+      }
+      if (bytesRead >= self.size && self.status === 'done') {
+        debug('write done');
+        return closeFD(e => next(e, null));
+      }
+      if (bytesRead + size >= self.size && self.status !== 'done') {
+        debug('file is writing, wait change');
+        return self.once('change', () => readFromFile(size, next))
+      }
+      if (fd !== undefined) return read(fd)
+      fs.open(self.filename, 'r', (err, _fd) => {
+        debug('open file to read');
+        if (err) return next(err);
+        fd = _fd;
+        debug('file is open');
+        return read(fd);
+      })
+
+      function read(fd) {
+        const length = Math.min(size, self.size - bytesRead);
+        fs.read(fd, Buffer.alloc(length), 0, length, null, (err, br, buffer) => {
+          if (err) {
+            return next(err)
+          }
+          bytesRead += br;
+          debug('read size:', br);
+          next(null, buffer)
+        })
+      }
+    })
   }
 }
 
